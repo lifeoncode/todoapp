3933529501197582ae20aa4cb7e84ba46c754ca8
lifeoncode <nduduzo@yellogarden.co.za>
2022-12-16 19:01:01 +0200

    todo routes ready

--- server/index.js
+++ server/index.js
@@ -10,6 +10,73 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
+// ROUTES
+
+// create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo(description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error("could not create todo:", err.message);
+  }
+});
+
+// get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error("could not get todos:", err.message);
+  }
+});
+
+// get a single todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error("could not get todo:", err.message);
+  }
+});
+
+// update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json("Todo updated successfully!");
+  } catch (err) {
+    console.error("could not update todo: ", err.message);
+  }
+});
+
+// delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json("Todo deleted successfully!");
+  } catch (err) {
+    console.error("could not delete todo: ", err.message);
+  }
+});
+
 // listen for connections on port 5000
 app.listen(5000, () => {
   console.log("SERVER RUNNING ON PORT: 5000");
